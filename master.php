deac187224d8a8f1dd5d66888a6bf10562b2f4d6
kanoriuta <kanoriuta@gmail.com>
2017-07-14 10:12:03 +0300

    Merger With Issue close?

--- master.php
+++ master.php
@@ -4,20 +4,10 @@ public function get() {
     
 }
 
-<<<<<<< HEAD
 public function set() {
     
 }
 
 public function test() {
-=======
-public function test() {
 
 }
-
-public function set() {
->>>>>>> origin/test
-    
-}
-
-?>
